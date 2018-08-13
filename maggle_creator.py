3368ce79fbf43b1666dee112bbfc588f043745b7
Shivam Bansal <shivam5992@gmail.com>
2018-08-13 23:19:54 +0800

    baseline changes

--- maggle_creator.py
+++ maggle_creator.py
@@ -1,34 +1,57 @@
+"""
+
+Maggle : a kaggle bot to create baseline models for different category of kaggle competitions or datasets
+
+__author__ == "shivam bansal"
+__email__ == "shivam5992@gmail.com"
+
+"""
+
+
+""" make necessary imports """
+
 import nbformat 
 import os
 
-nb = nbformat.v4.new_notebook()
-nb['cells'] = []
+class maggle():
+	def __init__(self, config):
+		self.nb = nbformat.v4.new_notebook()
+		self.nb['cells'] = []
+		self.config = config
+		self.meta_path = "Meta/"
 
-for x in sorted(os.listdir("Meta/")):
-	if x.startswith("."):
-		continue
+	def _prepare_meta(self):
+		pass
 
-	txt = ""
-	cod = ""
-	content = open("Meta/"+x).read()
-	for j, line in enumerate(content.split("\n")):
+	def _prepare(self):
+		for x in sorted(os.listdir(self.meta_path)):
+			if x.startswith("."):
+				continue
 
-		if line == "<text>":
 			txt = ""
-		if line == "</text>":
-			nb['cells'].append(nbformat.v4.new_markdown_cell(txt.replace("<text>","").replace("</text>","").strip()))
-			txt = ""
-
-		if line == "<code>":
 			cod = ""
-		if line == "</code>":
-			nb['cells'].append(nbformat.v4.new_code_cell(cod.replace("<code>","").replace("</code>","").strip()))
-			cod = ""
+			content = open(self.meta_path+x).read()
+			for j, line in enumerate(content.split("\n")):
 
-		txt += "\n"
-		txt += line 
-		cod += "\n"
-		cod += line 
+				if line == "<text>":
+					txt = ""
+				if line == "<code>":
+					cod = ""
 
-nbformat.write(nb, 'new.ipynb')
+				if line == "</text>":
+					txt = txt.replace("<text>","").replace("</text>","").strip()
+					self.nb['cells'].append(nbformat.v4.new_markdown_cell(txt))
+					txt = ""
 
+				if line == "</code>":
+					cod = cod.replace("<code>","").replace("</code>","").strip()
+					self.nb['cells'].append(nbformat.v4.new_code_cell(cod))
+					cod = ""
+
+				txt += "\n"
+				cod += "\n"
+
+				txt += line 
+				cod += line 
+
+		nbformat.write(self.nb, "baseline_kernel.ipynb")
