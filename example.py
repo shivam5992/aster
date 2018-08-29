b3eb90f197842acaaf8c304eac25bd3ce00102af
Shivam Bansal <shivam5992@gmail.com>
2018-08-30 00:25:22 +0800

    structured repo

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
 from aster.aster import aster
 
-config = {	"COMPETITION" : "spooky-author-identification",
-		  	"_TARGET_COL" : "author", 
-		  	"_ID_COL" : "id",
+config = {	"COMPETITION" : "titanic",
+		  	"_TARGET_COL" : "Survived", 
+		  	"_ID_COL" : "PassengerId",
 		  	"DATASET": "",
+		  	
 		  	# "_TRAIN_FILE" : "",
 		  	# "_TEST_FILE" : "",
 		  	# "_TAG" : "doc", # doc - text classification, num - binary classification 
